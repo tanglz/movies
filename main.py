9c3ee7df85187e1d9215a9949e3a7cf92fe4a933
Lisa Tang <lisatang@MacBook-Pro-3.local>
2021-02-20 13:30:39 -0500

    main

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from screenplay import get_screenplays_from_simply_scripts, get_screenplays_from
 
 if __name__ == '__main__':
     # get movie mate information
-    # movie_mate_info()
+    movie_mate_info()
 
     # # get screenplay
     # get movie titles
-    # titles = get_title_names()
+    titles = get_title_names()
     # save screenplay semi-structured information from website:simply_scripts
     # filename = get_screenplays_from_simply_scripts(titles)
 
     # save screenplay semi-structured information from website:daily_scripts
-    # filename = get_screenplays_from_daily_scripts(titles)
+    filename = get_screenplays_from_daily_scripts(titles)
 
     # batch download screenplay
     result = batch_download("scripts_daily.json")
